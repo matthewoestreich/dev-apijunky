77efd79d02d7b6b0a2af517c84a3fa9c556f51ef
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-09 17:45:39 -0600

    travis updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import 'module-alias/register';
+import { Server } from 'http';
 import express, { Application, Request, Response } from 'express';
 
-const port: number | string = process.env.PORT || 3000;
-const app: Application = express();
+const initializeExpress = (): Server => {
+    const port: number | string = process.env.PORT || 3000;
+    const app: Application = express();
 
-app.get('/', (req: Request, res: Response): void => {
-    res.send('Hello World!');
-});
+    app.get('/', (req: Request, res: Response): void => {
+        res.send('Hello World!');
+    });
 
-const server = app.listen(port);
+    return app.listen(port);
+};
 
+const server: Server = initializeExpress();
+
+let s: string;
+
+// eslint-disable-next-line import/prefer-default-export
 export { server };
