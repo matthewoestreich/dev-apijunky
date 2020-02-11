1cddaa3f4b6e49c264a5cd6af0e1ffde7d7bf45c
oze4 <matthewpoestreich@gmail.com>
2020-02-11 14:05:19 -0600

    Updates

--- src/middleware/errors.ts
+++ src/middleware/errors.ts
@@ -1,16 +1,14 @@
-import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
+import { ErrorRequestHandler, Request, Response } from 'express';
 
 import { CustomError } from 'errors';
 
-export const handleError: ErrorRequestHandler = (error, _req: Request, res: Response, _next: NextFunction): void => {
+export const handleError: ErrorRequestHandler = (error, _req: Request, res: Response): void => {
     console.error(error);
 
     const isErrorSafeForClient = error instanceof CustomError;
 
     const clientError = isErrorSafeForClient
-        ? {
-              ...error,
-          }
+        ? { ...error }
         : {
               message: 'Something went wrong, please contact our support.',
               code: 'INTERNAL_ERROR',
