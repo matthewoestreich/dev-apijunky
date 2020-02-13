0b43f22ab63b718a43e6ce537480182b36b60487
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-12 18:55:19 -0600

    User routes
    [1] Added user routes for various CRUD methods

--- src/controllers/api/v1/user.ts
+++ src/controllers/api/v1/user.ts
@@ -36,3 +36,39 @@ export const createNewUser = catchErrors(async (req: Request, res: Response) => 
         });
     }
 });
+
+export const findUser = catchErrors(async (req: Request, res: Response) => {
+    try {
+        const argsCount = Object.keys(req.query).length;
+        if (argsCount === 1 && req.query.un) {
+            const foundUser = await User.findOne({ username: req.query.un });
+            if (!foundUser) {
+                throw Error('Unable to find user!');
+            } else {
+                res.respond(200, { ...foundUser });
+            }
+        } else {
+            CustomError.toss('Incorrect parameter set!', {});
+        }
+    } catch (error) {
+        CustomError.toss('Unable to find user!', {});
+    }
+});
+
+export const validateUserPassword = catchErrors(async (req: Request, res: Response) => {
+    try {
+        const argsCount = Object.keys(req.query).length;
+        if (argsCount === 2 && req.query.un && req.query.pw) {
+            const foundUser = await User.findOne({ username: req.query.un });
+            if (!foundUser) {
+                throw Error('Unable to find user!');
+            } else {
+                res.respond(200, { status: foundUser.validatePassword(req.query.pw) });
+            }
+        } else {
+            CustomError.toss('Incorrect parameter set!', {});
+        }
+    } catch (error) {
+        CustomError.toss('Unable to find user!', {});
+    }
+});
