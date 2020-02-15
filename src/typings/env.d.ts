14624481abe8221c715669ca21323a96f2f6aea8
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-15 01:20:48 -0600

    Update env.d.ts

--- src/typings/env.d.ts
+++ src/typings/env.d.ts
@@ -1,12 +1,12 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        DB_HOST: string | 'localhost';
-        DB_PORT: string | '5432';
-        DB_USERNAME: string | '';
-        DB_PASSWORD: string | '';
-        DB_DATABASE: string | '';
-        JWT_ENCRYPTION_KEY: string | '';
-        JWT_SIGNING_KEY: string | '';
-        JWT_EXPIRES_IN: string | '';
+        DB_HOST: string;
+        DB_PORT: string;
+        DB_USERNAME: string;
+        DB_PASSWORD: string;
+        DB_DATABASE: string;
+        JWT_ENCRYPTION_KEY: string;
+        JWT_SIGNING_KEY: string;
+        JWT_EXPIRES_IN: string;
     }
 }
