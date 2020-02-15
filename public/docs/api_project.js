358932df7c49a32073b6c4217bf45074d023c3fb
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-14 20:58:33 -0600

    Update

--- public/docs/api_project.js
+++ public/docs/api_project.js
@@ -1,15 +1,15 @@
 define({
   "name": "[ts-api]",
   "version": "0.0.1",
-  "description": "API Documentation",
-  "title": "This is a test repo for learning how to do things the right way!",
+  "description": "This is a repo for learning how to do this the right way!",
+  "title": "ts-api docs",
   "url": "https://tin-fire.herokuapp.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-02-15T02:36:37.490Z",
+    "time": "2020-02-15T02:53:44.501Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
